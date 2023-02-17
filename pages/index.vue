33c050834d96c38ec84c98139a454122d5538757
Mathew A. K <kramer.mathew.a@gmail.com>
2023-02-17 13:50:42 -0500

    Add header and responsive background

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,44 @@
 <template>
-  <Header></Header>
+  <div class="wrapper">
+    <Header></Header>
+    <Home></Home>
+  </div>
 </template>
+
+<style>
+
+@import url('https://fonts.googleapis.com/css2?family=Red+Hat+Mono:ital,wght@0,400;0,700;1,400&display=swap');
+
+body {
+  height: 100vh;
+  background: rgb(164,32,109);
+  background: linear-gradient(90deg, rgba(164,32,109,1) 50%, rgba(255,255,255,1) 50%);
+
+  font-family: 'Red Hat Mono', monospace;
+}
+
+@media screen and (max-width: 767px) {
+  body {
+    background: rgb(164,32,109);
+    background: linear-gradient(180deg, rgba(164,32,109,1) 50%, rgba(255,255,255,1) 50%);
+  }
+}
+
+#__nuxt {
+  height: 100%;
+}
+
+#__layout {
+  height: 100%;
+}
+
+.wrapper {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+}
+
+</style>
 
 <script lang="ts">
 import Vue from 'vue'
