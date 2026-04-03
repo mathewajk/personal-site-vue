d375c28000b5448ea95fdc71e5994ebb5f80dab5
mathew <kramer.mathew.a@gmail.com>
2026-04-03 14:03:04 -0400

    Fix scroll position on route change

--- src/router.ts
+++ src/router.ts
@@ -35,4 +35,10 @@ const routes = [
 export const router = createRouter({
   history: createWebHistory(),
   routes,
+  scrollBehavior(_to, _from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition
+    }
+    return { top: 0 }
+  },
 })
